43aacc2673315fc6ce4ee5aee497c52e7226a741
Jordi Vilà <jordi.vila@airalo.com>
2026-03-25 08:16:33 +0100

    Move countries server route to its folder

--- server/api/countries/countries.get.ts
+++ server/api/countries/countries.get.ts
@@ -1,7 +1,6 @@
 const uuidv4: string | null = "ed536916-dfd9-41a7-bcde-247887cc6b52";
 
 export default defineEventHandler(async () => {
-  
-  const response = await $fetch(`https://airalo.com/api/v2/countries`);
+  const response = await $fetch(`https://www.airalo.com/api/v2/countries`);
   return response
 });
